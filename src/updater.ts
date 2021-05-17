387e79714468906d2bdac700781b6951429ec069
mpaulweeks <mpaulweeks@gmail.com>
2021-05-17 14:59:52 -0400

    draft simple-git integration

---
+++ src/updater.ts
@@ -0,0 +1,20 @@
+import simpleGit, { SimpleGit } from 'simple-git';
+
+export class Updater {
+  readonly timeout = 1 * 60 * 1000; // 1 minute
+
+  run() {
+    const sg: SimpleGit = simpleGit();
+    sg.exec(() => console.log('Starting pull...'))
+      .pull((err, update) => {
+        if (update && update.summary.changes) {
+          require('child_process').exec('npm restart');
+        }
+      })
+      .exec(() => console.log('pull done.'));
+  }
+
+  cron() {
+    setInterval(() => this.run(), this.timeout);
+  }
+}
