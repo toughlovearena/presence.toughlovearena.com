481bb6038ae03895f0ec71181b606bdb5a82c6b0
mpaulweeks <mpaulweeks@gmail.com>
2021-05-17 19:56:23 -0400

    fix kill issue by spawning childProcess as detached

--- src/updater.ts
+++ src/updater.ts
@@ -1,24 +1,22 @@
 import childProcess from 'child_process';
-import fs from 'fs';
 import simpleGit, { SimpleGit } from 'simple-git';
 
 export class Updater {
   private readonly timeout = 1 * 5 * 1000; // 1 minute
   private rebuilding = false;
-  private testfile = fs.readFileSync('testfile').toString();
 
   async run() {
     if (this.rebuilding) { return; }
 
-    const newTestfile = fs.readFileSync('testfile').toString();
-
     const sg: SimpleGit = simpleGit();
     const pullResult = await sg.pull();
-    const changes = (pullResult.summary.changes > 0) || (newTestfile !== this.testfile);
+    const changes = (pullResult.summary.changes > 0);
 
     if (changes) {
       this.rebuilding = true;
-      await childProcess.exec('npm run rebuild');
+      await childProcess.spawn('npm run rebuild', {
+        detached: true,
+      });
     }
   }
 
